db226195d07f1049ed7fe80f7f97c494b3112252
Đỗ Khắc Quyền <quyendk.b24dtcn380@stu.ptit.edu.vn>
2025-02-26 16:21:38 +0700

    Update index07.js

--- lession7/index07.js
+++ lession7/index07.js
@@ -1,4 +1,5 @@
 let math = 10;
 let physics = 8;
 let chemistry = 8.5;
-document.write((math + physics + chemistry) / 3);
+let avg = ((math + physics + chemistry) / 3).toFixed(2);
+document.write(avg);

f930af539be1e58ef67a9773582559ce471025ec
Đỗ Khắc Quyền <quyendk.b24dtcn380@stu.ptit.edu.vn>
2025-02-26 15:53:18 +0700

    Update index10.js

--- lession10/index10.js
+++ lession10/index10.js
@@ -1,7 +1,4 @@
-// let number1 = parseInt(prompt("Nhập số thứ nhất: "));
-// let number2 = parseInt(prompt("Nhập số thứ hai: "));
-// let random = Math.floor(Math.random() * (number1 - number2 + 1)) + number2;
-// document.write(`Số random giữa ${number1} và ${number2} là: ${random}`);
+
 let num1 = parseFloat(prompt("Nhập số thứ nhất:"));
 let num2 = parseFloat(prompt("Nhập số thứ hai:"));
 
